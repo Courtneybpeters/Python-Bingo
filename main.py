0c26b2a2600a97c87643bc444c602eae6b57d306
Courtney Peters <courtneybpeters@gmail.com>
2015-07-27 08:50:15 -0500

    - changed import for naming purposes - correct screen refresh to pygame main loop

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import player as p
-import game as g
-import bingocard as bc
-import bingoblower as bb
+from player import Player
+from game import Game
+from bingocard import BingoCard
+from bingoblower import BingoBlower
 import pygame, sys
 from pygame.locals import *
+
 
 """
 BINGO:
@@ -14,44 +15,58 @@ BINGO:
 """
 def main():
     pygame.init()
+    fpsClock = pygame.time.Clock()
+
     screen = pygame.display.set_mode((800, 480))
     pygame.display.set_caption('Pyngo')
 
-    # TODO: Need player input on very beginning menu
-    player = p.Player("Courtney")
-    game = g.Game(player)
+    # TODO: Need player input on very beginning start_menu
+    player = Player("Courtney")
+    game = Game(player)
+
+    state = 'new game'
 
     while True:
+        if state == 'new game':
+            game.start_menu(screen)
+
+
+
+
+
         screen.fill(game.bg_color)
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.event.post(pygame.event.Event(QUIT))
+
+        pygame.display.update()
+        fpsClock.tick(30)
 
     print
     print "Welcome to Courtney's Pyngo - Bingo in Python."
-    choice = game.menu()
+    choice = game.start_menu()
 
     if choice == 1:
-        card = bc.BingoCard()
+        card = BingoCard()
         card.draw_bingocard()
     elif choice == 2:
         # I'm letting player handle everything
         # Would I want average so I can send it to other classes possibly?
-        p.calculate_average()
+        calculate_average()
 
-    blower = bb.BingoBlower()
+    blower = BingoBlower()
 
     for i in range(5):
         # TODO: WAITING/TIMING INPUT FUNCTION
         game.selected.append(blower.get_ball())
         game.ball_call()
 
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-        elif event.type == KEYDOWN:
-            if event.key == K_ESCAPE:
-                pygame.event.post(pygame.event.Event(QUIT))
 
-    pygame.display.update()
 
 if __name__ == "__main__":
     main()
